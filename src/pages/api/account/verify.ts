b704309591c27e5041222e12dcfdc70a000542d5
julienbrg <julien@strat.cc>
2023-08-29 03:03:19 +0200

    add check

--- src/pages/api/account/verify.ts
+++ src/pages/api/account/verify.ts
@@ -1,18 +1,44 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { SiweMessage } from 'siwe'
 import { withSessionRoute } from '../../../utils/server'
+import { ethers } from 'ethers'
+import { NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI } from '../../../lib/consts'
 
 export default withSessionRoute(async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.method, '/api/account/verify', req.session)
 
+  const verifyNftOwnership = async (addr) => {
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc-test.arthera.net')
+    const nft = new ethers.Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, provider)
+    const nftBal = await nft.balanceOf(addr)
+    console.log('nftBal:', nftBal)
+    if (Number(nftBal) > 0) {
+      return true
+    }
+  }
+
+  const decrypt = async (msg) => {
+    // TODO: process.env.NEXT_PUBLIC_SECRET
+    return 'https://bafybeiebgibloauyw6jynmbma2jrfaykqgvcd2p4gxbgm5pyjzvainndw4.ipfs.w3s.link/old-book.png'
+  }
+
   if (req.method === 'POST') {
     try {
       const { message, signature } = req.body
+      console.log('message.address', message.address)
       const siweMessage = new SiweMessage(message)
       const fields = await siweMessage.validate(signature)
-
       if (fields.nonce !== req.session.nonce) return res.status(422).json({ message: 'Invalid nonce.' })
 
+      const isNftOwner = await verifyNftOwnership(message.address)
+      if (!isNftOwner) return res.status(422).json({ message: 'Could not verify the NFT ownership.' })
+      let secretContent
+      if (isNftOwner) {
+        secretContent = await decrypt('hello')
+      } else {
+        secretContent = 'oh!'
+      }
+      // TODO: write the decrypted message in the response
       req.session.siwe = fields
       await req.session.save()
       return res.json({ ok: true })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,21 @@
-import { Heading, Button } from '@chakra-ui/react'
+import { Heading, Button, Image, useToast } from '@chakra-ui/react'
 import { Head } from '../components/layout/Head'
-// import Image from 'next/image'
 import { LinkComponent } from '../components/layout/LinkComponent'
+import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import { useFeeData, useSigner, useAccount, useBalance, useNetwork } from 'wagmi'
 import { ethers } from 'ethers'
 import { NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI } from '../lib/consts'
-import useSound from 'use-sound' // https://www.joshwcomeau.com/react/announcing-use-sound-react-hook/
-const stevie = 'https://bafybeicxvrehw23nzkwjcxvsytimqj2wos7dhh4evrv5kscbbj6agilcsy.ipfs.w3s.link/another-star.mp3'
 
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(false)
   const [userBal, setUserBal] = useState<string>('')
   const [txLink, setTxLink] = useState<string>('')
 
+  const router = useRouter()
+  const toast = useToast()
   const { data } = useFeeData()
   const { address, isConnecting, isDisconnected } = useAccount()
-
   const { data: signer } = useSigner()
   const {
     data: bal,
@@ -27,10 +26,6 @@ export default function Home() {
   })
   const network = useNetwork()
 
-  const [play, { stop, pause }] = useSound(stevie, {
-    volume: 0.5,
-  })
-
   const explorerUrl = network.chain?.blockExplorers?.default.url
 
   const nft = new ethers.Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, signer)
@@ -38,11 +33,19 @@ export default function Home() {
   useEffect(() => {
     const val = Number(bal?.formatted).toFixed(3)
     setUserBal(String(val) + ' ' + bal?.symbol)
-  }, [bal?.formatted, bal?.symbol, address])
+    const verifOneTime = verifyNftOwnership(address)
+    if (verifOneTime) {
+      router.push('/access')
+    }
+  }, [bal?.formatted, bal?.symbol, address, router])
 
-  const checkFees = () => {
-    console.log('data?.formatted:', JSON.stringify(data?.formatted))
-    return JSON.stringify(data?.formatted)
+  const verifyNftOwnership = async (addr) => {
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc-test.arthera.net')
+    const nft = new ethers.Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, provider)
+    const nftBal = await nft.balanceOf(addr)
+    if (Number(nftBal) > 0) {
+      return true
+    }
   }
 
   const mint = async () => {
@@ -54,8 +57,30 @@ export default function Home() {
       console.log('tx:', nftReceipt)
       setTxLink(explorerUrl + '/tx/' + nftReceipt.transactionHash)
       setLoading(false)
-      play()
+      console.log('Minted. ✅')
+      toast({
+        title: 'Successful mint',
+        position: 'bottom',
+        description: 'You have the Whitepaper NFT on your wallet!',
+        status: 'success',
+        variant: 'subtle',
+        duration: 500,
+        isClosable: true,
+      })
+      router.push('/access')
     } catch (e) {
+      if ((e.data.message = 'execution reverted: Caller already minted')) {
+        toast({
+          title: 'Already minted',
+          position: 'bottom',
+          description: "You can't mint this one twice.",
+          status: 'error',
+          variant: 'subtle',
+          duration: 1000,
+          isClosable: true,
+        })
+      }
+
       setLoading(false)
       console.log('error:', e)
     }
@@ -66,44 +91,42 @@ export default function Home() {
       <Head />
 
       <main>
-        <Heading as="h2">Basic Minter</Heading>
-        <br />
-        <p>Welcome to Basic Minter!</p>
-
+        {/* <Heading as="h2">Basic Minter</Heading> */}
         {isDisconnected ? (
           <>
             <br />
-            <p>Please connect your wallet if you want to mint.</p>
+            <p>Please connect your wallet.</p>
           </>
         ) : (
           <>
             <br />
 
-            <p>You&apos;re about to mint 1 NFT on Ethereum Goerli Testnet.</p>
+            <p>
+              We&apos;re living pretty exciting times. <LinkComponent href="/access">Discover Atrthera Whitepaper right now!</LinkComponent>
+            </p>
             <br />
             <p>
-              You&apos;re connected to <strong>Ethereum Goerli Testnet</strong> and your wallet currently holds
-              <strong> {userBal}</strong>. You can go ahead and click on the &apos;Mint&apos; button below: you will be invited to sign your
-              transaction.{' '}
+              You&apos;re connected to <strong>Arthera Testnet</strong> and your wallet currently holds
+              <strong> {userBal}</strong>. You can go ahead and click on the &apos;Mint&apos; button below:{' '}
             </p>
-          </>
-        )}
+            <br />
 
-        <br />
-        {!loading ? (
-          !txLink ? (
-            <Button colorScheme="green" variant="outline" onClick={mint}>
-              Mint
-            </Button>
-          ) : (
-            <Button disabled colorScheme="green" variant="outline" onClick={mint}>
-              Mint
-            </Button>
-          )
-        ) : (
-          <Button isLoading colorScheme="green" loadingText="Minting" variant="outline">
-            Mint
-          </Button>
+            {!loading ? (
+              !txLink ? (
+                <Button colorScheme="green" variant="outline" onClick={mint}>
+                  Mint
+                </Button>
+              ) : (
+                <Button disabled colorScheme="green" variant="outline" onClick={mint}>
+                  Mint
+                </Button>
+              )
+            ) : (
+              <Button isLoading colorScheme="green" loadingText="Minting" variant="outline">
+                Mint
+              </Button>
+            )}
+          </>
         )}
 
         {txLink && (
